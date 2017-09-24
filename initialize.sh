69b5f6b97cbdee1a82e81d9ca1118801e0abdfba
hMatoba <hiroaki.mtb@outlook.com>
2017-09-24 22:42:38 +0900

    ドメイン名対応の修正

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-envsubst '$$LINK_NAME' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
+envsubst '$$LINK_NAME,$$DOMAIN' < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
 certbot certonly --standalone -t -d ${DOMAIN} -m ${E_MAIL} --agree-tos -n
 nginx
 crond -f
